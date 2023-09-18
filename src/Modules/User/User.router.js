009d0021b548110556c32cdc6138bc406a0ae9fd
OsaidAttar <osedatar2017@gmail.com>
2023-09-18 03:00:51 +0300

    profile

--- src/Modules/User/User.router.js
+++ src/Modules/User/User.router.js
@@ -7,7 +7,7 @@ import { auth, roles } from '../../MiddelWare/auth.middelware.js';
 import { endpoint } from './User.endPoint.js';
 const router =Router()
 router.post('/profile/:userId',validation(validtors.profile),fileUpload(fileValidation.image).single('image'),userController.userProfile)
-router.put('/profile/update/:userId',auth(endpoint.update),validation(validtors.updateProfile),fileUpload(fileValidation.image).single('image'),userController.updateUserProfile)
+router.put('/profile/update/:userId',auth(endpoint.update),fileUpload(fileValidation.image).single('image'),validation(validtors.updateProfile),userController.updateUserProfile)
 router.delete('/profile/delete/:userId',auth(endpoint.delete),fileUpload(fileValidation.image).single('image'),userController.deleteUserProfile)
 router.patch('/updatepassword',auth(endpoint.update),validation(validtors.updatePassword),userController.updatePassword)
 router.get('/:id/shareprofile',auth(Object.values(roles)),validation(validtors.shareProfile),userController.shareProfile);
